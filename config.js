209ce5d93e7510a6851b53a96b585aa601d2c94f
Hirdeshwar20 <80970821+Hirdeshwar20@users.noreply.github.com>
2022-07-25 20:39:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+     apiKey: "AIzaSyArx0dZAg17Zf-CLWwd0qmVk8p9KGzH5qw",
+  authDomain: "storytelling-d700e.firebaseapp.com",
+  databaseURL: "https://storytelling-d700e-default-rtdb.firebaseio.com",
+  projectId: "storytelling-d700e",
+  storageBucket: "storytelling-d700e.appspot.com",
+  messagingSenderId: "596638470297",
+  appId: "1:596638470297:web:0a42af8959ba61e6666808"
+};
